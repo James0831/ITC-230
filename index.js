a388afd4f6c56f0838366978459aafad852f4aaf
James0831 <jdcarroll08@gmail.com>
2017-07-11 16:28:02 -0700

    Updated for assignment 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var http = require("http"), fs = require('fs');
+var http = require("http"), fs = require('fs'), qs = require("querystring");
+var songs = require("./lib/songs.js");
 
 function serveStatic(res, path, contentType, responseCode){
   if(!responseCode) responseCode = 200;
@@ -16,18 +17,40 @@ function serveStatic(res, path, contentType, responseCode){
 }
 
 http.createServer(function(req,res){
-  var path = req.url.toLowerCase();
+  var url = req.url.split("?"); // separate route from query string
+  var params = qs.parse(url[1]); // conver query string to object
+  var path = url[0].toLowerCase();
+  
   switch(path) {
     case '/': 
       serveStatic(res, '/public/home.html', 'text/html');
       break;
     case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About');
+      serveStatic(res, '/public/about.html', 'text/html');
       break;
+    case '/get':
+      var song = songs.get(params.artist);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      var result = (song) ? JSON.stringify(song) : 'Could not find that artist';
+      res.end('The results for ' + params.artist + ' are: \n' + result);
+      break;
+    case '/delete':
+      var song = songs.delete(params.artist);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      var result = (song) ? JSON.stringify(song) : 'Could not find that artist';
+      res.end(result);
+      break;
+      /*
+    case '/add':
+      var song = songs.add(params.newSong);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('The new artist added is: ' + params.artist)
+      */
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('404:Page not found.');
   }
   
 }).listen(process.env.PORT || 3000);
+
+
