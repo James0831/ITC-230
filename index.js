068351c3292bdcb29cf0f6c98d77ddab66ccaeff
James0831 <jdcarroll08@gmail.com>
2017-07-13 18:02:28 -0700

    Assignment 3

--- index.js
+++ index.js
@@ -1,56 +1,68 @@
-var http = require("http"), fs = require('fs'), qs = require("querystring");
-var songs = require("./lib/songs.js");
+'use strict'
+let song = require("./lib/songs.js");
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  console.log(__dirname + path)
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
+const express = require("express");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+// set up handlebars view engine
+var handlebars = require('express-handlebars');
+app.engine('.html', handlebars({extname: '.html'}));
+app.set('view engine', '.html');
+
+// home page send a static file
+app.get('/', function(req, res){
+  res.type('text/html');
+  res.render('home', {songs: song.getAll()});
+});
+
+// about page
+app.get('/about', function(req, res){
+  res.type('text/html');
+  res.render('about');
+});
+
+// details page
+app.get('/details', function(req, res){
+  console.log(req.query)
+  var found = song.get(req.query.artist);
+  res.render("details", {artist: req.query.artist, result: found, songs: song.getAll()});
+});
+
+app.post('/details', function(req,res){
+    console.log(req.body)
+    var found = song.get(req.body.artist);
+    res.render("details", {artist: req.body.artist, result: found, songs: song.getAll()});
+});
+
+// handle GET 
+app.get('/delete', function(req,res){
+    let result = song.delete(req.query.artist); // delete book object
+    res.render('delete', {title: req.query.artist, result: result});
+});
+
+
+
+
+
+
+// custom 404 page
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error;');
   });
-}
-
-http.createServer(function(req,res){
-  var url = req.url.split("?"); // separate route from query string
-  var params = qs.parse(url[1]); // conver query string to object
-  var path = url[0].toLowerCase();
   
-  switch(path) {
-    case '/': 
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-    case '/about':
-      serveStatic(res, '/public/about.html', 'text/html');
-      break;
-    case '/get':
-      var song = songs.get(params.artist);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      var result = (song) ? JSON.stringify(song) : 'Could not find that artist';
-      res.end('The results for ' + params.artist + ' are: \n' + result);
-      break;
-    case '/delete':
-      var song = songs.delete(params.artist);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      var result = (song) ? JSON.stringify(song) : 'Could not find that artist';
-      res.end(result);
-      break;
-      /*
-    case '/add':
-      var song = songs.add(params.newSong);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('The new artist added is: ' + params.artist)
-      */
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404:Page not found.');
-  }
-  
-}).listen(process.env.PORT || 3000);
-
-
+app.listen(app.get('port'), function(){
+  console.log( 'Express started');
+});
