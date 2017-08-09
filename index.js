e51977f86c090ec18e037577ff3617d139424d9e
james0831 <jdcarroll08@gmail.com>
2017-08-08 18:10:50 -0700

    Week 7 REST API's

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var Song = require("./models/song");
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+app.use('/api', require('cors')());
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars');
@@ -28,7 +29,7 @@ app.get('/about', (req,res) => {
   res.render('about');
 });
 
-// details page hanles GET
+// details page handles GET
 app.get('/details', (req,res,next) => {
   Song.findOne({ artist:req.query.artist }, (err,song) => {
     if (err) return next(err);
@@ -64,11 +65,71 @@ app.get('/add', (req,res) => {
     res.render('added', {title: req.query.artist, result: result});
 });
 
+//api's
+
+// get a single item
+app.get('/api/v1/song/:artist', (req,res,next) => {
+  var artist = req.params.artist;
+  console.log(artist);
+  Song.findOne({ artist:artist }, (err,results) => {
+    if (err ||  !results) return next(err);
+    res.json(results);
+  });
+});
+
+//get all items
+app.get('/api/v1/songs', (req,res, next) => {
+    Song.find((err,results) => {
+        if (err || !results) return next(err);
+        res.json(results);
+    });
+});
+
+// delete song
+app.get('/api/v1/delete/:artist', (req,res, next) => {
+    Song.remove({"artist":req.params.artist }, (err, result) => {
+        if (err) return next(err);
+        // return # of items deleted
+        res.json({"deleted": result.result.n});
+    });
+});
+
+//adds a song
+app.post('/api/v1/add/', (req,res, next) => {
+    // find & update existing item, or add new 
+    if (!req.body._id) { // insert new document
+        let song = new Song({artist:req.body.artist,title:req.body.title,album:req.body.album});
+        song.save((err,newSong) => {
+            if (err) return next(err);
+            console.log(newSong)
+            res.json({updated: 0, _id: newSong._id});
+        });
+    } else { // update existing document
+        Song.updateOne({ _id: req.body._id}, {artist:req.body.artist, title: req.body.title, album: req.body.album }, (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
+});
+//adds a song
+app.get('/api/v1/add/:artist/:title/:album', (req,res, next) => {
+    // find & update existing item, or add new 
+    let artist = req.params.artist;
+    Song.update({ artist: artist}, {artist:artist, title: req.params.title, album: req.params.album }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        // nModified = 0 for new item, = 1+ for updated item 
+        res.json({updated: result.nModified});
+    });
+});
+
+
+
+
 // custom 404 page
 app.use((req, res) => {
   res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.send('404 - Page Not Found');
 });
 
 // custom 500 page
